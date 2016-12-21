b34300ac33c225f690454ef6541a152bc3348aca
Luke <lukecleavland@gmail.com>
2016-12-21 12:28:36 -0600

    validation

--- js/signup.js
+++ js/signup.js
@@ -1,57 +1,48 @@
+function submit_form() {
 
+	$('#email').submit(function(e)
+	{
+		if ($(".required").val().length > 0)
+		{
+			var email = $("#emailinput").val();
+			
+			$.ajax({
+			type:'POST',
+			data: {email:email},
+			url: "http://foodfightmarketing.com/KioskApp/formPros.php",
+			
+				success:function(data)
+				{
+					console.log(data);		
+					$("#email").html("");
+					$(".message").show().html("<span style='color:green'>Your email address has been added! <br /> Thank you for taking the time to fill out this form.</span>");
+					//window.setTimeout(window.location.reload(),5000);
+					//alert('Your comment was successfully added');
+				},
+				error:function(){
+					$(".message").show().html("<span style='color:red'>There was an error connecting to the database.</span>");
+				
+				}
+			});
+		}else{
+			$(".message").show().html("<span style='color:red'>Email address cannot be blank!</span>");
+		}
+	e.preventDefault();
+	
+	})
+	
+	$("#emailinput").focus(function(){
+		
+		$(".message").hide()
+	})
+}
 $(document).ready(function(){
-
+	submit_form();
 	$(window).keydown(function(event){
 		if(event.keyCode == 13) {
 		event.preventDefault();
 		$("#email").blur();
 		}
 	});
-	
-	$('#submit').click(function(e){
-	var postData = $("#email").serialize();
-	/*
-	   if ($("#email").val().length<=1){
-			$("#email").css("background","yellow");
-			return false;
-	   }else{
-	   */
-		   $.ajax({
-		type:'POST',
-		data: postData,
-		url: "http://foodfightmarketing.com/KioskApp/v3/formPros.php",
-		
-
-		success:function(data)
-		{
-		
-		 alert(postData); 
-		 //$("#ff")[0].reset();
-		 $("form").html(data);
-		//window.setTimeout("window.location.reload()",5000);
-		//alert('Your comment was successfully added');
-
-		  //return true;
-		},
-		error:function(){
-			$(".warning").text("There was an error connecting to the database");
-			//alert('There was an error adding your comment');
-
-			//window.setTimeout("window.location.reload()", 5000);
-		}
-			});
-		
-	  e.preventDefault();
-	  
-	 //  }
-	
-	})
-//})
-	
-	$("#email").focus(function(){
-				$("#email").css("background-color","white");
-			});
-
-	
 
 	});
